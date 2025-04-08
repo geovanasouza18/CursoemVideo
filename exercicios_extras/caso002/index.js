95445c18b92f809dc50ed43edf73a5edbd340247
Geovana Souza <geovanasouza0712@gmail.com>
2025-04-07 22:35:42 -0300

    novas atividades

---
+++ exercicios_extras/caso002/index.js
@@ -0,0 +1,6 @@
+let nome = "Carlos";
+let idade = 28;
+let altura = 1.82;
+let temAlibi = false;
+
+alert(`Carlos tem ${idade}, mede ${altura} e tem o álibi: ${temAlibi}`)
---
+++ exercicios_extras/caso003/index.js
@@ -0,0 +1,9 @@
+let x = "Desconhecido";
+let y = 0;
+let z = false;
+let w;
+
+alert(`A variável x é do tipo: ${typeof x}`)
+alert(`A variável y é do tipo: ${typeof y}`)
+alert(`A variável z é do tipo: ${typeof z}`)
+alert(`A variável w é do tipo: ${typeof w}`)
