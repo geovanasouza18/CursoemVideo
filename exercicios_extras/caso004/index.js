135baf475c503d0bc7c7c07d3d6862353e181957
Geovana Souza <geovanasouza0712@gmail.com>
2025-04-09 22:39:45 -0300

    Caso 004

---
+++ exercicios_extras/caso004/index.js
@@ -0,0 +1,15 @@
+/* let senha = "1234"; // senha começa como string
+
+// // Convertendo para número
+ senha = Number(senha); 
+ Faltou fazer isso */
+
+let senha = 1234
+
+alert(`A variável é ${typeof senha}`)
+
+if (senha === 1234){
+    alert("Acesso liberado!")
+}else{
+    alert("Erro de conversão.")
+}
