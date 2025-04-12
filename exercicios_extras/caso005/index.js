d2a9821d7c0cbdec0da43a3e847777320bed44f5
Geovana Souza <geovanasouza0712@gmail.com>
2025-04-11 22:28:57 -0300

    Atualizações

---
+++ exercicios_extras/caso005/index.js
@@ -0,0 +1,6 @@
+let suspeito1 = 42;
+let suspeito2 = "42";
+
+window.alert(`O resultado usando o == foi ${suspeito1 == suspeito2}`)
+
+window.alert(`O resultado usando o === foi ${suspeito1 === suspeito2}`)
--- exercicios_extras/caso_001/index.js
+++
@@ -1,6 +0,0 @@
-let nome = "Joana"
-let idade = 32
-let altura = 1.75
-let suspeito = true
-
-alert(`A suspeita se chama ${nome}, tem ${idade} anos e sua altura é de ${altura} metros`)
