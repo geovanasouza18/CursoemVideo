bacbdcdc8a235b747193ec6b6f1550299394e74f
Geovana Souza <geovanasouza0712@gmail.com>
2025-04-14 22:34:46 -0300

    entrada de cinema

---
+++ exercicios_extras/ex02/index.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("Digite sua idade"))
+let ingresso = prompt("Possui o ingresso?")
+
+if(idade >=16 && ingresso ==="sim"){
+    alert("Pode entrar, aproveite o filme!")
+}else {
+    alert("Você não pode entrar no cinema")
+}
