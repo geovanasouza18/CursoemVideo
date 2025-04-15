421bd323e6adac9ad74ce178f6441cd39efe75ec
Geovana Souza <geovanasouza0712@gmail.com>
2025-04-14 22:22:36 -0300

    Adivinhar número

---
+++ exercicios_extras/ex01/index.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um numero!"))
+
+if (numero >4) {
+    window.alert("O número é maior do que o chute")
+}else if(numero <4) {
+    window.alert("O número é menor do que o chute!") 
+}else{
+    window.alert("Você acertou o número!")
+}
