4bfa2744d1717a272d4727999f6573de56b91cf4
Geovana Souza <geovanasouza0712@gmail.com>
2025-08-04 22:38:07 -0300

    aprendendo sonbre

---
+++ extras/extra01.py
@@ -0,0 +1,10 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+nota4 = float(input('Digite a quarta nota: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f'A média do aluno(a) é: {media:.2f}')
+
+n = input('Digite algo: ')
+print(n.isalpha())
