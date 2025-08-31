25581c86542d84b4014759b998f795d5760a7d74
Geovana Souza <geovanasouza0712@gmail.com>
2025-08-31 10:35:10 -0300

    Exercicios

--- desafios/desafio020.py
+++ desafios/desafio020.py
@@ -5,4 +5,4 @@ aluno3 = input("Digite o nome do terceiro aluno: ")
 aluno4 = input("Digite o nome do quarto aluno: ")
 lista = [aluno1, aluno2, aluno3, aluno4]
 random.shuffle(lista)
-print("O nome sorteado foi {}".format(lista))
+print("A Ordem sorteada foi {}".format(lista))
