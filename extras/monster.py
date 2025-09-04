48d59096cbe62ce20b35bd87133bfe8805eacb27
Geovana Souza <geovanasouza0712@gmail.com>
2025-09-03 22:19:49 -0300

    desafio 02 - OBC

---
+++ extras/monster.py
@@ -0,0 +1,6 @@
+nameMonster = input("Digite o nome do seu monstro: ")
+typeMonster = input("Digite o tipo do seu monstro (ex: dragão, goblin, zumbi): ")
+levelMonster = int(input("Digite o nível do seu monstro: "))
+skillMonster = input("Digite a habilidade do seu monstro: ")
+
+print(f"O monstro {nameMonster}, é um {typeMonster} de nível {levelMonster} e possui a habilidade especial: {skillMonster}.")
