f209b8369056ba6ded1fd11711aefdcc220a10b2
Geovana Souza <geovanasouza0712@gmail.com>
2025-09-23 22:24:51 -0300

    revisão

--- desafios/desafio022.py
+++ desafios/desafio022.py
@@ -1,4 +1,6 @@
 nome = str(input("Digite seu nome completo: ")).strip()
 print(nome.upper())
 print(nome.lower())
-print(len(nome.replace(" ", "")))
+print(len(nome))
+dividido = nome.split()[0]
+print(len(dividido))
