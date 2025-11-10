70471c2608278e1e877d6a3f7b8e00f402800615
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-09 21:11:54 -0300

    Desaio 54 - mundo 2 python

--- desafios/desafio054.py
+++ desafios/desafio054.py
@@ -1,6 +1,16 @@
 from datetime import date
 
-for ano in range(0, 7):
-    idade = int(input('Digite seu ano de nascimento: '))
-    atual = date.today().year - idade
-    if atual >= 21
+maiores = 0
+menores = 0
+
+for pessoa in range(0, 7):
+    nascimento = int(input('Digite o ano de nascimento: '))
+    idade = date.today().year - nascimento
+
+    if idade >= 21:
+        maiores += 1
+    else:
+        menores += 1
+
+print(f'\nAo todo tivemos {maiores} pessoas maiores de idade.')
+print(f'E também tivemos {menores} pessoas menores de idade.')
