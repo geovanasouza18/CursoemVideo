19ab5a62d641083e7b99fcb0f77ba72e56ae9eb5
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-14 10:11:33 -0300

    atualizações

--- desafios/desafio057.py
+++ desafios/desafio057.py
@@ -1,4 +1,4 @@
-sexo = str(input('Digite seu sexo: ')).strip().upper()
+sexo = str(input('Digite seu sexo: ')).strip().upper()[0]
 while sexo != 'F' and sexo != 'M':
-    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
 print(f'Sexo registrado com sucesso!')
