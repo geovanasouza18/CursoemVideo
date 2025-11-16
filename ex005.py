7efb9485563a5d0db2edada444e07e6c9797882e
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-15 21:32:12 -0300

    treinando o loop while

---
+++ ex005.py
@@ -0,0 +1,23 @@
+#Caso 1 — O Cofre que Só Abre com Contagem
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+print('Cofre aberto 🔓')
+
+#Caso 2 — O Suspeito das Tentativas Infinitas
+senha = input('Insira sua senha: ')
+while senha != '1234':
+    senha = input('Senha incorreta! Digite novamente: ')
+print('Acesso concedido')
+
+#Caso 3 — A Fuga do Número Fantasma
+contador2 = 50
+while contador2 > 0:
+    print(contador2)
+    contador2 -= 1
+print('Sumiu na neblina')
+
+#Caso 4 — O Interrogatório dos Ímpares
+while
+    print('Suspeito encontrado 🔎')
