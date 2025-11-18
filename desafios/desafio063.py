3bdd55035c0596d5ed57075bd8b7cccc9787f1b5
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-17 21:44:49 -0300

    resolvidos

--- desafios/desafio063.py
+++ desafios/desafio063.py
@@ -1,3 +1,11 @@
-num = int(input('Digite um número inteiro: '))
-sequencia = 0
-
+n = int(input('Quantos termos você quer mostrar: '))
+t1 = 0
+t2 = 1
+cont = 3 #porque já foi mostrado o 1 e segundo termo
+while cont <= n:
+    t3 = t1 + t2
+    print(t3, end=' -> ')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print('FIM')
