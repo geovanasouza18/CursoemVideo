27c6cb0b54a5aa6f9fe19f3af07d3a7cd910d341
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-19 21:53:58 -0300

    exemplos

---
+++ ex006.py
@@ -0,0 +1,11 @@
+print('Número de 1 a 5 e multiplicando por 2 usando o for')
+for i in range(1,6):
+    print(i * 2)
+
+print('Número de 1 a 5 e multiplicando por 2 usando o while')
+contador = 0
+while contador <= 5:
+    print(contador * 2)
+    contador += 1
+
+
