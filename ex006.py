0c7a4d7d85817369a33ae1a4317d1fef63a698ea
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-19 22:08:23 -0300

    praticando

--- ex006.py
+++ ex006.py
@@ -8,4 +8,13 @@ while contador <= 5:
     print(contador * 2)
     contador += 1
 
+print('Usando o break')
+
+contador2 = 0
+while True:
+    print(contador2 * 2)
+    contador2 += 1
+    if contador2 == 20:
+        break
+
 
