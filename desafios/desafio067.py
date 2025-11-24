a1c12c36f3fe9db8a35cbbb1066e683748318b55
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-23 21:56:37 -0300

    Atualizações

--- desafios/desafio067.py
+++ desafios/desafio067.py
@@ -1,3 +1,13 @@
-num = int(input('Digite um número para ver a tabuada: '))
-mult = 0
+print('-' * 30)
+print('TABUADA — DESAFIO 67')
+print('-' * 30)
 while True:
+    num = int(input('Quer ver a tabuada de qual número? '))
+    if num < 0:
+        break
+    print('-' * 30)
+    for c in range(1, 11):
+        print(f'{num} x {c} = {num * c}')
+    print('-' * 30)
+
+print('PROGRAMA ENCERRADO. VOLTE SEMPRE!')
