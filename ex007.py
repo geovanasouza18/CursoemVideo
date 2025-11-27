8d045068a0b8e90a1351b3954ad6a3f347fbda8c
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-26 22:19:04 -0300

    novos exercícios

---
+++ ex007.py
@@ -0,0 +1,8 @@
+#CASO 01 — O Cofre do Sr. Holmes
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+print(f'A soma dos números {num1} + {num2} = {num1 + num2}')
+print(f'A subtração dos números {num1} - {num2} = {num1 - num2}')
+print(f'A multiplicação dos números {num1} * {num2} = {num1 * num2}')
+print(f'A divisão dos números {num1} / {num2} = {num1 / num2:.2f}')
+
