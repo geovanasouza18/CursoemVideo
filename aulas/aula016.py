8955ca3169ff47cc109d505cab796057a29ab8ac
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-28 09:25:48 -0300

    tuplas

--- aulas/aula016.py
+++ aulas/aula016.py
@@ -1,8 +1,23 @@
-lanche = ('Burger', 'Juice', 'Pizza', 'Pudding') #Tuplas são imutáveis
+lanche = ('Burger', 'Juice', 'Pizza', 'Pudding', 'Chips') #Tuplas são imutáveis
+print(lanche)
 print(lanche[0:2])
 print(lanche[1:])
 print(lanche[-2])
 print(lanche[0::2])
 print(len(lanche))
+
+#Forma 1
 for i in lanche:
     print(i)
+
+print('=' * 30)
+
+#Forma 2
+for pos, i in enumerate (lanche):
+    print(i , pos)
+
+print('=' * 30)
+
+#Forma 3
+for cont in range(0, len(lanche)):
+    print(lanche[cont], cont)
