e4dada6a294341c3475b1a4102dfad221396af9f
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-28 09:34:36 -0300

    tuplas

--- aulas/aula016.py
+++ aulas/aula016.py
@@ -27,3 +27,16 @@ print('=' * 30)
 #Forma 3
 for cont in range(0, len(lanche)):
     print(lanche[cont], cont)
+
+#Juntando Tuplas
+print('=' * 30)
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c)
+print(c.count(5))
+print(c.index(5))
+
+pessoa = ('João', 21, '2003', 'Brasil')
+del (pessoa)
+print(pessoa)
