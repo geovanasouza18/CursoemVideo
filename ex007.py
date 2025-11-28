1536b20157778379e39fb9123102ef47202eff0b
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-27 22:32:51 -0300

    revisando While

--- ex007.py
+++ ex007.py
@@ -6,3 +6,12 @@ print(f'A subtração dos números {num1} - {num2} = {num1 - num2}')
 print(f'A multiplicação dos números {num1} * {num2} = {num1 * num2}')
 print(f'A divisão dos números {num1} / {num2} = {num1 / num2:.2f}')
 
+# Caso 02 — O Cliente Misterioso
+const = 0
+while True:
+    preco = int(input('Digite um valor: '))
+    if preco < 0:
+        break
+    const += preco
+
+print(f'O total da soma dos números foi: {const}')
