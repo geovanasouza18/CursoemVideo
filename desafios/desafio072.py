b484d516f201f3e71a77abcc4222164047fbc82e
Geovana Souza <geovanasouza0712@gmail.com>
2025-11-29 22:35:18 -0300

    atualizações

--- desafios/desafio072.py
+++ desafios/desafio072.py
@@ -1,7 +1,1 @@
 extenso = ('Zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-while True:
-    num = int(input('Digite um numero entre 0 e 20: '))
-    while not (0 <= num <= 20):
-        num = int(input('Tente novamente. Digite um numero entre 0 e 20: '))
-    if num >= 0 and num <= 20:
-        print
