5452e34f281aa42cfeea607babdd57de2316e44d
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-01 09:28:05 -0300

    desafio concluído

---
+++ desafios/desafio076.py
@@ -0,0 +1,20 @@
+print('-' * 35)
+print('       LISTAGEM DE PREÇOS')
+print('-' * 35)
+
+listaProd = (
+    'Lápis', 1.75,
+    'Borracha', 2.00,
+    'Caderno', 15.90,
+    'Estojo', 25.00,
+    'Transferidor', 4.20,
+    'Compasso', 9.99,
+    'Mochila', 120.32,
+    'Canetas', 22.30,
+    'Livro', 34.90
+)
+
+for i in range(0, len(listaProd), 2):
+    produto = listaProd[i]
+    preco = listaProd[i + 1]
+    print(f'{produto:.<25} R${preco:>6.2f}')
