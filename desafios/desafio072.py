b6577f10091c0f7949e099f817a10c76bbd56164
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-01 08:47:52 -0300

    desafio concluído

--- desafios/desafio072.py
+++ desafios/desafio072.py
@@ -1,1 +1,7 @@
-extenso = ('Zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+extenso = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+texto = extenso[num]
+print(texto)
