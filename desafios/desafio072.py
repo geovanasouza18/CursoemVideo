48d642f0191b12a1e75a1f387273528e2155cddc
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-01 21:59:18 -0300

    alterações

--- desafios/desafio072.py
+++ desafios/desafio072.py
@@ -3,5 +3,6 @@ while True:
     num = int(input('Digite um número entre 0 e 20: '))
     if 0 <= num <= 20:
         break
+    print('Tente novamente. ', end='')
 texto = extenso[num]
 print(texto)
