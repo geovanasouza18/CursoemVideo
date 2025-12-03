59829555de002efa7f0eae0127a7435db8cec053
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-03 08:07:08 -0300

    desafio concluído

--- desafios/desafio077.py
+++ desafios/desafio077.py
@@ -1,2 +1,8 @@
 palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+
 for palavra in palavras:
+    print(f'Na palavra {palavra.upper()} temos ', end='')
+    for letra in palavra:
+        if letra in 'aeiou':
+            print(f'{letra}', end=' ')
+    print()
