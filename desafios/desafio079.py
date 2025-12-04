72e9c9104e62b8b926a2532504484fcd43fa46d7
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-03 22:15:02 -0300

    desafio concluído

--- desafios/desafio079.py
+++ desafios/desafio079.py
@@ -1,1 +1,17 @@
-list()
+valores = []
+while True:
+    valor = int(input('Digite um valor: '))
+    # Usando o 'if' para verificar a duplicidade
+    if valor not in valores:
+        valores.append(valor)
+        print(f'Valor adicionado com sucesso....')
+    else:
+        print(f'Valor duplicado na lista. Não vou adicionar.')
+
+    opcao = ' '
+    while opcao not in 'SN': #enquanto não for uma dessas opções, vai perguntar eternamente
+        opcao = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if opcao == 'N':
+        break
+valores.sort()
+print(f'Você digitou os valores {valores}')
