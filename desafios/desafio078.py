1c0d384b05d554d26922127c9b8ecf6ccee4893b
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-04 21:31:33 -0300

    desafio concluído

--- desafios/desafio078.py
+++ desafios/desafio078.py
@@ -1,9 +1,22 @@
-valores =list()
+valores = []
+
 for cont in range(0, 5):
     valores.append(int(input('Digite um valor: ')))
-    maior = max(valores)
-    menor = min(valores)
-print(f'Você digitou os valores {valores}')
 
-print(f'O maior número foi {maior} nas posições ')
-print(f'O menor número foi {menor} nas posições ')
+maior = max(valores)
+menor = min(valores)
+
+# listas para guardar todas as posições
+posicaoMaior = []
+posicaoMenor = []
+
+# percorre a lista inteira e coleta as posições
+for posicao, valor in enumerate(valores):
+    if valor == maior:
+        posicaoMaior.append(posicao)
+    if valor == menor:
+        posicaoMenor.append(posicao)
+
+print(f'Você digitou os valores {valores}')
+print(f'O maior número foi {maior} nas posições {posicaoMaior}')
+print(f'O menor número foi {menor} nas posições {posicaoMenor}')
