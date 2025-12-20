5549f1e7b32862b07187a39e7b11a11e78303cca
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-19 22:31:16 -0300

    desafio em andamento

--- desafios/desafio084.py
+++ desafios/desafio084.py
@@ -1,1 +1,33 @@
-#desafio 84
+quant = maior_peso = menor_peso = 0
+galera = []
+dados = []
+while True:
+    dados.append(str(input('Nome: ')))
+    dados.append(int(input('Peso: ')))
+    galera.append(dados[:])
+    dados.clear()
+    quant += 1
+
+    opcao = ' '
+    while opcao not in 'SN': #enquanto não for uma dessas opções, vai perguntar eternamente
+        opcao = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if opcao == 'N':
+        break
+
+peso_max = max(pessoa[1] for pessoa in galera)
+peso_min = min(pessoa[1] for pessoa in galera)
+
+print(f'Ao todo, você cadastrou {quant} pessoas.')
+
+print(f'O maior peso foi de {peso_max}Kg. Peso de ', end='')
+for pessoa in galera:
+    if pessoa[1] == peso_max:
+        print(pessoa[0], end=' ')
+
+
+print(f'\nO menor peso foi de {peso_min}Kg. Peso de ', end='')
+for pessoa in galera:
+    if pessoa[1] == peso_min:
+        print(pessoa[0], end=' ')
+
+
