4352d43ac40d48764513dc0118130b7c25bcc63a
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-20 21:50:32 -0300

    desafio concluído

--- desafios/desafio085.py
+++ desafios/desafio085.py
@@ -2,10 +2,11 @@ valores = []
 pares = []
 impares = []
 for num in range(1, 8):
-    valores.append(int(input(f'Digite o {num}º valor: ')))
-if valores % 2 == 0:
-    pares.append(valores[:])
-else:
-    impares.append(valores[:])
-print(f'Os valores pares digitados foram {pares}')
-print(f'Os valores impares digitados foram {impares}')
+    num = (int(input(f'Digite o {num}º valor: ')))
+    valores.append(num)
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+print(f'Os valores pares digitados foram {sorted(pares)}')
+print(f'Os valores impares digitados foram {sorted(impares)}')
