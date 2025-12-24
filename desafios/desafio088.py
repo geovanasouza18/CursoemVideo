f2e2ac40fa036b913b23d41e984cb19d282c3d8f
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-23 22:20:47 -0300

    desafio concluído

--- desafios/desafio088.py
+++ desafios/desafio088.py
@@ -1,6 +1,33 @@
 from random import randint
-import time
-jogo = list()
+from time import sleep
+lista = list()
+jogos = list()
+print('-'*30)
+print(f'{"JOGA NA MEGA SENA":^30}')
+print('-'*30)
 
-sorteio = randint(1,60)
+quant = int(input('Quantos jogos deseja jogar: '))
+total = 1
+while total <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
+
+
+
+
+
+
 
