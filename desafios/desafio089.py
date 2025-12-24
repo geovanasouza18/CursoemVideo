7ece6eea6a6061f5440896f5a3b58381c6b08b79
Geovana Souza <geovanasouza0712@gmail.com>
2025-12-23 22:34:06 -0300

    desafio concluído

--- desafios/desafio089.py
+++ desafios/desafio089.py
@@ -14,3 +14,11 @@ print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
 for i, a in enumerate(ficha):
     print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
 
+while True:
+    print('-='*30)
+    opc = int(input("Mostrar notas de qual aluno? (999 interrompe): "))
+    if opc == 999:
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< Volte sempre >>>')
