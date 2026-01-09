5b224953c6d5dee6ea645bb326b61795d5a83d20
Geovana Souza <geovanasouza0712@gmail.com>
2026-01-09 09:35:56 -0300

    desafio concluído

--- desafios/desafio090.py
+++ desafios/desafio090.py
@@ -1,1 +1,10 @@
 #Desafio 90
+alunos = {}
+alunos['Nome'] = str(input('Digite o nome do aluno: '))
+alunos['Media'] = float(input('Digite a média do aluno: '))
+if alunos['Media'] >= 7:
+    alunos['Situacao'] = 'Aprovado'
+else:
+    alunos['Situacao'] = 'Reprovado'
+for k, v in alunos.items():
+    print(f'{k} é igual a {v}')
