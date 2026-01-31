04bfb228a30ae301a849e9223fe06f34ac2a0ff1
Geovana Souza <geovanasouza0712@gmail.com>
2026-01-30 21:12:31 -0300

    atualizações

--- desafios/desafio091.py
+++ desafios/desafio091.py
@@ -1,6 +1,8 @@
 from random import randint
 import time
-sorteio = randint(1, 6)
-jogador = dict()
+jogadores = {'jogador1': randint(1, 6), 'jogador2': randint(1, 6), 'jogador3': randint(1, 6), 'jogador4': randint(1, 6),}
 
+for k, v in jogadores.items():
+    time.sleep(1)
+    print(f'O jogador {k} tirou {v}')
 
