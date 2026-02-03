7cd9ada040cb852ef362b115e6984eca618557fd
Geovana Souza <geovanasouza0712@gmail.com>
2026-02-03 08:45:29 -0300

    aula sobre função

---
+++ aulas/aula020.py
@@ -0,0 +1,22 @@
+#Sem usar a função
+print('-='*20)
+print('Curso em Vídeo')
+print('-='*20)
+print('-='*20)
+print('Aprendendo Função em Python')
+print('-='*20)
+print('-='*20)
+print('Com o Prof. Gustavo Guanabara')
+print('-='*20)
+
+#Com a função
+def criar_linha():
+    print('-='*20)
+
+
+print('Curso em Vídeo - usando função')
+criar_linha()
+print('Aprendendo Função em Python')
+criar_linha()
+print('Com o Prof. Gustavo Guanabara')
+criar_linha()
